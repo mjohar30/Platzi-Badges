9c7bc170a64de049f337fd495964c5fed059cb72
Mario Johar <mjohar30@gmail.com>
2019-09-14 23:10:03 -0500

    Creación de modal

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -2,12 +2,22 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import Badge from '../components/Badge'
+import DeleteBadgeModal from '../components/DeleteBadgeModal'
 import confLogo from '../images/platziconf-logo.svg'
 
 import './styles/BadgeDetails.css'
 
-function BadgeDetails(props) {
+function useIncreaseCount(max){
+    const [ count, setCount] = React.useState(0)
+    if (count > max){
+        setCount(0)
+    }
 
+    return [count, setCount]
+}
+
+function BadgeDetails(props) {
+    const [ count, setCount] = useIncreaseCount(4)
     const badge = props.badge
 
     return (
@@ -39,14 +49,24 @@ function BadgeDetails(props) {
                         <h2>Actions</h2>
                         <div>
                             <div>
+                                <button onClick={() => {
+                                    setCount(count + 1);
+                                }} className="btn btn-primary mr-4">
+                                    Increse Count: {count}
+                                </button>
                                 <Link className="btn btn-primary mb-4" to={`badges/${badge.id}/edit`}>
                                     Edit
                                 </Link>
                             </div>
                             <div>
-                                <button className="btn btn-danger">
+                                <button onClick={props.onOpenModal} className="btn btn-danger">
                                     Delete
                                 </button>
+                                <DeleteBadgeModal
+                                    isOpen={props.modalIsOpen}
+                                    onClose={props.onCloseModal}
+                                    onDeleteBadge={props.onDeleteBadge}
+                                />
                             </div>
                         </div>
                     </div>
